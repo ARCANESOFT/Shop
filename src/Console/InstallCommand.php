2f41b00f319eae5b5cd91f979d2ac122a2f3c13a
ARCANEDEV <arcanedev.maroc@gmail.com>
2021-07-29 22:39:44 +0100

    WIP

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -1,10 +1,26 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Arcanesoft\Shop\Console;
 
-class InstallCommand
+use Illuminate\Console\Command;
+
+/**
+ * Class     InstallCommand
+ *
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InstallCommand extends Command
 {
-    //
+    /* -----------------------------------------------------------------
+     |  Main Methods
+     | -----------------------------------------------------------------
+     */
+
+    /**
+     * Handle the command.
+     */
+    public function handle(): int
+    {
+        return static::SUCCESS;
+    }
 }
--- src/Console/PublishCommand.php
+++ src/Console/PublishCommand.php
@@ -1,10 +1,23 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Arcanesoft\Shop\Console;
 
-class PublishCommand
+use Arcanesoft\Foundation\Support\Console\PublishCommand as Command;
+
+/**
+ * Class     PublishCommand
+ *
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class PublishCommand extends Command
 {
-    //
+    /**
+     * Handle the command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        return static::SUCCESS;
+    }
 }
